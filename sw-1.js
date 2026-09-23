1bd513a9280b1ead9316b34e2b33e5d9fcd1f6c7
soniaaguilar0000-create <sonia.aguilar.0000@gmail.com>
2026-09-23 19:03:15 +0200

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,22 @@
+const CACHE="plan-nutricional-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.webmanifest",
+        "./icon.svg"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
